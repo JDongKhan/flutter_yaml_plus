982f361a3f27fbad192d1b3969f5d8d579353e75
JD <419591321@qq.com>
2024-06-07 10:54:32 +0800

    push

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'package:yaml_modify/yaml_modify.dart';
 
 import 'src/config_file.dart';
 
-const version = '1.0.4';
+const version = '1.0.7';
 const String fileOption = 'file';
 const String urlOption = 'url';
 const String helpFlag = 'help';
---
+++ push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+version=""
+#支持外部传version
+while [ -n "$1" ]
+do
+    case "$1" in
+        -version) version="$2"
+            shift ;;
+        --) shift
+        break ;;
+         *) echo "$1 is not an option" ;;
+    esac
+    shift
+done
+
+sed -i '' "s/const version = .*;/const version = '$version';/g" lib/main.dart
+dart compile exe bin/flutter_yaml_plus.dart -o pub+
+
+git add .
+git commit -m "push"
