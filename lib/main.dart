e3f049e193017810fe4f4ea248eb2d36ae468b4b
JD <419591321@qq.com>
2024-06-12 13:29:18 +0800

    push 1.2.1

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ import 'package:yaml_modify/yaml_modify.dart';
 
 import 'src/config_file.dart';
 
-const version = '1.0.4';
+const version = '1.2.1';
 const String fileOption = 'file';
 const String urlOption = 'url';
 const String helpFlag = 'help';
